8bcd12819a27cb021eea886b1c2b833493e08b4b
odaialazzeh <odai.alazzeh@gmail.com>
2023-01-18 21:06:33 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import App from './App';
+import './index.css';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+);
