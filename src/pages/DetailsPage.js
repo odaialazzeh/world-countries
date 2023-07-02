736cc32216eddf8c4602295016546ad9e371dc16
unknown <odai.alazzeh@gmail.com>
2023-07-02 20:01:23 +0300

    Update pages

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,77 +1,150 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import DetailsHeader from '../components/NavBar/DetailsHeader';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useNavigate, Link } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+import { countryAction } from '../redux/countryNameReducer';
+import Loader from '../components/Loader';
+import HomeHeader from '../components/NavBar/HomeHeader';
 
-const CountryDetails = () => {
-  const location = useLocation();
-  const data = location.state;
+const CountryDetail = () => {
+  const dispatch = useDispatch();
+  const { cca3 } = useParams();
+  const navigate = useNavigate();
 
+  const countryList = useSelector((state) => state.countryList);
+  const { country, loading } = countryList;
+
+  useEffect(() => {
+    dispatch(countryAction(cca3));
+  }, [dispatch, cca3]);
+
+  // Find the specific country based on the country code
+  const countryFind = country.find((cnt) => cnt.cca3 === cca3);
+  const languageKeys = countryFind?.languages
+    ? Object.keys(countryFind.languages)
+    : [];
+
+  const btnHandler = () => {
+    navigate('/');
+  };
   return (
     <div>
-      <DetailsHeader countryName={data.name} />
-      <div>
-        <div className="container-details">
-          <div className="top-data">
-            <img src={data.flag} alt={data.name} className="details-img" />
-            <div>
-              <h2>
-                Native name:
-                <span className="data">{data.nativeName}</span>
-              </h2>
-              <h2>
-                Continent:
-                <span className="data">{data.region}</span>
-              </h2>
-            </div>
-          </div>
+      <HomeHeader />
+      <button type="button" className="btn-details" onClick={btnHandler}>
+        Back
+      </button>
+      {loading && <Loader />}
+      {!loading && countryFind && (
+        <div className="main-container">
+          <section className="main_section">
+            <section className="left">
+              <img
+                className="rounded"
+                src={countryFind.flags.png}
+                alt={countryFind.name.common}
+              />
+            </section>
+            <section className="right">
+              <h3>{countryFind.name.common}</h3>
+              <main>
+                <div>
+                  <p>
+                    <span>Native Name: </span>
+                    <strong>
+                      {languageKeys.length > 0
+                        ? countryFind.name.nativeName[languageKeys[0]].official
+                        : ''}
+                    </strong>
+                  </p>
 
-          <div className="details-text">
+                  <p>
+                    <span>Continent: </span>
+                    <strong>{countryFind.continents}</strong>
+                  </p>
 
-            <h3>
-              Official Name:
-              <span className="data">{data.name}</span>
-            </h3>
-            <h3>
-              Capital:
-              <span className="data">{data.capital}</span>
-            </h3>
-            <h3>
-              Sub-Region:
-              <span className="data">{data.subregion}</span>
-            </h3>
-            <h3>
-              Area:
-              <span className="data">
-                {data.area}
-                 &nbsp; km2
-              </span>
-            </h3>
-            <h3>
-              Population:
-              <span className="data">
-                {(data.population / 1000000).toFixed(1)}
-                &nbsp; million
-              </span>
-            </h3>
-            <h3>
-              Time Zone:
-              <span className="data">
-                {data.timezones}
-              </span>
-            </h3>
-            <h3>
-              Location:
-              <span className="data">
-                <a href={`https://maps.google.com/?q=${data.latlng[0]},${data.latlng[1]}`}>Click here</a>
-                {data.maps}
-              </span>
-            </h3>
+                  <p>
+                    <span>Population: </span>
+                    <strong>
+                      {countryFind.population >= 1000000
+                        ? `${(
+                          countryFind.population / 1000000
+                        ).toLocaleString()} million`
+                        : `${(
+                          countryFind.population / 1000
+                        ).toLocaleString()} thousand`}
+                    </strong>
+                  </p>
 
-          </div>
+                  <p>
+                    <span>Region: </span>
+                    <strong>{countryFind.region}</strong>
+                  </p>
+
+                  <p>
+                    <span>Sub Region: </span>
+                    <strong>{countryFind.subregion}</strong>
+                  </p>
+
+                  <p>
+                    <span>Capital: </span>
+                    <strong>{countryFind?.capital}</strong>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <span>Currency: </span>
+                    <strong>
+                      {Object.values(countryFind.currencies)[0]?.name}
+                    </strong>
+                  </p>
+                  <p>
+                    <span>Currency Symbol: </span>
+                    <strong style={{ fontSize: '1rem' }}>
+                      {Object.values(countryFind.currencies)[0]?.symbol}
+                    </strong>
+                  </p>
+                  <p>
+                    <span>
+                      Languages:
+                      <br />
+                    </span>
+                    <strong>
+                      {Object.values(countryFind.languages).map((lang) => (
+                        <React.Fragment key={uuidv4()}>
+                          {lang}
+                          {', '}
+                        </React.Fragment>
+                      ))}
+                      {' '}
+                    </strong>
+                  </p>
+
+                  <p>
+                    <span>Map: </span>
+                    <a style={{ textDecoration: 'none' }} href={countryFind.maps.googleMaps}>Google Maps Link</a>
+                  </p>
+                </div>
+              </main>
+              <div>
+                <h3 className="label">Border Countries: </h3>
+                <div className="btn_border">
+                  {countryFind.borders ? (
+                    countryFind.borders.map((border) => (
+                      <span key={uuidv4()} id="border">
+                        <Link to={`/alpha/${border}`}>{border}</Link>
+                      </span>
+                    ))
+                  ) : (
+                    <p>There is no border for this country</p>
+                  )}
+                </div>
+              </div>
+            </section>
+          </section>
         </div>
-      </div>
+      )}
     </div>
   );
 };
 
-export default CountryDetails;
+export default CountryDetail;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import HomeHeader from '../components/NavBar/HomeHeader';
-import { getCountries } from '../redux/countries';
+import { listCountries } from '../redux/countriesReducer';
 import CountryList from '../components/countries/CountriesList';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
 
 const CountriesHome = () => {
   const dispatch = useDispatch();
-  const countries = useSelector((state) => state.countriesData.countries);
+  const countriesList = useSelector((state) => state.countriesList);
+  const { countries, loading, error } = countriesList;
 
   const [searchCountry, setSearchCountry] = useState(' ');
 
   useEffect(() => {
-    if (countries.length === 0) {
-      dispatch(getCountries());
-    }
-  }, [dispatch, countries.length]);
+    dispatch(listCountries());
+    setSearchCountry('');
+  }, [dispatch]);
 
-  const filteredCountries = countries.filter((country) => (
-    country.name.toLowerCase().includes(searchCountry.toLowerCase())
-    || country.region.toLowerCase().includes(searchCountry.toLowerCase())));
-  if (!Array.isArray(countries)) {
-    return <div>Loading...</div>;
-  }
+  const filteredCountries = countries.filter((country) => country.name.common.toLowerCase().includes(searchCountry.toLowerCase()));
+
+  const noMatchesFound = searchCountry.length > 0 && filteredCountries.length === 0;
 
   const handleSearch = (e) => {
     e.preventDefault();
@@ -31,19 +30,31 @@ const CountriesHome = () => {
   return (
     <div>
       <HomeHeader />
-      <div className="container">
-        <div className="search-box">
-          <input
-            type="text"
-            placeholder="Search Country"
-            name="searchCountry"
-            onChange={handleSearch}
+      <div className="search">
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M17.112 15.059h-1.088l-.377-.377a8.814 8.814 0 002.15-5.784A8.898 8.898
+                 0 008.898 0 8.898 8.898 0 000 8.898a8.898 8.898 0
+                 008.898 8.899c2.211 0 4.23-.808
+                 5.784-2.143l.377.377v1.081l6.845 6.832
+                 2.04-2.04-6.832-6.845zm-8.214 0A6.16 6.16 0 118.9 2.737a6.16 6.16 0 010 12.322z"
           />
-        </div>
-
-        <CountryList countries={searchCountry.length ? filteredCountries : countries} />
-
+        </svg>
+        <input
+          type="text"
+          placeholder="Search by Country"
+          name="searchCountry"
+          onChange={handleSearch}
+          value={searchCountry}
+        />
       </div>
+      {loading && <Loader />}
+      {noMatchesFound && <Error searchValue={searchCountry} />}
+      {!loading && !error && !noMatchesFound && (
+        <CountryList
+          countries={searchCountry.length ? filteredCountries : countries}
+        />
+      )}
     </div>
   );
 };
